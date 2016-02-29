4b879500dfede3f996f3ed83bf8e4b0bd1033e85
Developer <>
2016-02-29 13:31:24 -0430

    move the project outside of the OF directory

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,4 @@
-export OF_PATH=..
+export OF_PATH=../openFrameworks-master
 export arch=x86_64
-export DYLD_FRAMEWORK_PATH=../libs/glut/lib/osx
-export DYLD_LIBRARY_PATH=../libs/fmodex/lib/osx
+export DYLD_FRAMEWORK_PATH=$OF_PATH/libs/glut/lib/osx
+export DYLD_LIBRARY_PATH=$OF_PATH/libs/fmodex/lib/osx
